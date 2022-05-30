1969a317a042505c74f30754157979a2c4aa0708
Git Class <paultorres1993@gmail.com>
2022-05-30 13:57:43 -0400

    completed part-one-api

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 
+
 const app = express()
 
+app.set('view engine', 'pug')
 
+app.get('/', (request, response) => {
+  response.render('index',)
+})
+
+app.all('*', (request, response) => {
+  return response.sendStatus(404)
+})
 
 app.listen(1337, () => {
   console.log('Listening on http://localhost:1337..') // eslint-disable-line no-console
